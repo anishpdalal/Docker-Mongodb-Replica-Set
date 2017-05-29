fb0d754d54507bf7f0c9079bc89fad8442b91d1b
Anish Dalal <anishdalal1@gmail.com>
2017-05-28 22:35:01 -0400

    Only initiate replica set with mongo node residing in primary instance

--- config/replica.js
+++ config/replica.js
@@ -2,9 +2,7 @@ rs.initiate(
   {
     _id: 'curriculumReplSet',
     members: [
-      {_id: 0, host: 'manager:27017'},
-      {_id: 1, host: 'workerA:27017'},
-      {_id: 2, host: 'workerB:27017'}
+      {_id: 0, host: 'manager:27017'}
     ]
   }
 )
