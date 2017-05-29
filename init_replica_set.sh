77e67b8704b23b46db4daab8ff169d8177f51723
Anish Dalal <anishdalal1@gmail.com>
2017-05-29 02:57:59 -0400

    Add a bash script function that spins up container, adds config files to container, then is shutdown

--- init_replica_set.sh
+++ init_replica_set.sh
@@ -1,4 +1,25 @@
 # Step 1) - Create Docker Volume
+# Step 2) - Create Primary Member of Replica Set
+
+function switchVM {
+  env='docker-machine env '$1
+  echo 'switched to $1'
+  eval $($env)
+}
+
+# @params server container volume
+function createContainer {
+  # start container
+  docker run --name $1 -v $2:/data -d mongo --smallfiles
+
+}
+
+# @params container volume
+function addConfigFilesToContainer {
+  echo 'Starting container '$1
+
+}
+
 
 # @params volume
 function createDockerVolume {
